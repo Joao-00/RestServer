98a2c38a2e3f151aad0931489c0440f49948f205
Joao Ormeño <joao23.1996@gmail.com>
2022-05-05 22:14:24 -0400

    validacion de campos

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -7,7 +7,7 @@ const { request } = require('express');
 const {response} = require('express');
 const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
-const { validationResult } = require('express-validator');
+
 
 
 const usuariosGet = (req = request, res = response) => {
@@ -26,12 +26,6 @@ const usuariosGet = (req = request, res = response) => {
 }
 
 const usuariosPost = async (req, res = response) => {
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors);
-    }
-
 
 
     const {nombre, correo, password, rol} = req.body;
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -8,6 +8,7 @@ const { usuariosGet,
         usuariosPost, 
         usuariosDelete, 
         usuariosPatch } = require('../controllers/usuarios');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 
 const router = Router();
@@ -17,7 +18,11 @@ const router = Router();
 router.get('/', usuariosGet );
 router.put('/:id', usuariosPut );
 router.post('/',[
+        check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+        check('password', 'El password es obligatorio y debe ser mas de 6 letras').isLength({min: 6}),
         check('correo', 'El correo no es valido').isEmail(),
+        check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+        validarCampos
 ], usuariosPost );
 router.patch('/', usuariosPatch );
 router.delete('/', usuariosDelete );
