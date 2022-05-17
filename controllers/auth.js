7974370b1cf6fe3a82b6a1830c211ebfa6e3b6ed
Joao Ormeño <joao23.1996@gmail.com>
2022-05-17 15:16:12 -0400

    login usuario - trabajando en jwt

---
+++ controllers/auth.js
@@ -0,0 +1,54 @@
+const bcryptjs = require("bcryptjs");
+const { response } = require("express");
+const Usuario = require('../models/usuario');
+
+
+const login = (req, res = response) => {
+
+    const {correo, password} = req.body;
+
+    try {
+
+        //TODO: verificar si el email existe
+        const usuario = await Usuario.findOne({correo});
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'Usuario / password no son correctos - correo'
+            });
+        }
+
+        //TODO: si el usuario esta activo
+        if (!usuario.estado) {
+            return res.status(400).json({
+                msg: 'Usuario / password no son correctos - estado: false'
+            });
+        }
+
+        //TODO: verificar la password
+        const validPassword = bcryptjs.compareSync(password, usuario.password);
+        if (!validPassword) {
+            return res.status(400).json({
+                msg: 'Usuario / password no son correctos - password'
+            });
+        }
+
+        //TODO: generar el JWT
+
+
+        res.json({
+            msg: 'Login ok',
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        })
+    }
+
+}
+
+
+module.exports = {
+    login
+}
