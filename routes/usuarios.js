f2f9460f2c46fa7cb274035d38d697be0dd52e6d
Joao Ormeño <joao23.1996@gmail.com>
2022-05-19 20:52:09 -0400

    validacion rol administrador para eliminar

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -4,6 +4,9 @@ const { check } = require('express-validator');
 
 
 const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const { esAdminRole } = require('../middlewares/validar-roles');
+
 const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 
 const { usuariosGet,
@@ -11,7 +14,6 @@ const { usuariosGet,
         usuariosPost,
         usuariosDelete,
         usuariosPatch } = require('../controllers/usuarios');
-const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
@@ -37,6 +39,7 @@ router.post('/',[
 
 router.delete('/:id',[
     validarJWT,
+    esAdminRole,
     check('id', 'No es un ID válido').isMongoId(),
     check('id').custom( existeUsuarioPorId ),
     validarCampos
