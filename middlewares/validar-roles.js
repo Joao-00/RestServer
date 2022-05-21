c129e1f5d083386eb1f326a55bc122fd5bc44b1a
Joao Ormeño <joao23.1996@gmail.com>
2022-05-21 13:35:26 -0400

    middlewares tieneRoles | optimizando importaciones

--- middlewares/validar-roles.js
+++ middlewares/validar-roles.js
@@ -21,6 +21,29 @@ const esAdminRole = (req, res = response, next) => {
 }
 
 
+
+const tieneRole = ( ...roles) => {
+    return(req, res = response, next) => {
+        //console.log(roles, req.usuario.rol);
+
+        if(!req.usuario){
+            return res.status(500).json({
+                msg: 'se quiere verificar el role sin validar el token primero'
+            });
+        }
+
+        if (!roles.includes(req.usuario.rol)) {
+            return res.status(401).json({
+                msg: `El servicio requiere uno de estos roles ${roles}`
+            });
+        }
+
+        next();
+    }
+}
+
+
 module.exports = {
-    esAdminRole
+    esAdminRole,
+    tieneRole
 }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,10 +2,10 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-
-const { validarCampos } = require('../middlewares/validar-campos');
-const { validarJWT } = require('../middlewares/validar-jwt');
-const { esAdminRole } = require('../middlewares/validar-roles');
+const {validarCampos, 
+        validarJWT, 
+        esAdminRole, 
+        tieneRole} = require('../middlewares')
 
 const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 
@@ -40,6 +40,8 @@ router.post('/',[
 router.delete('/:id',[
     validarJWT,
     esAdminRole,
+    //valida roles
+    tieneRole('ADMIN_ROLE', 'VENTAS_ROLE'),
     check('id', 'No es un ID válido').isMongoId(),
     check('id').custom( existeUsuarioPorId ),
     validarCampos
