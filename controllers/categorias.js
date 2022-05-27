163a3b9466d5d59a1ce8c822393b769776924f32
Joao Ormeño <joao23.1996@gmail.com>
2022-05-27 18:26:29 -0400

    obtener categoria

--- controllers/categorias.js
+++ controllers/categorias.js
@@ -2,6 +2,36 @@ const {response} = require('express');
 const {Categoria} = require('../models');
 
 
+const obtenerCategorias = async(req, res = response ) => {
+
+    const { limite = 5, desde = 0 } = req.query;
+    const query = { estado: true };
+
+    const [ total, categorias ] = await Promise.all([
+        Categoria.countDocuments(query),
+        Categoria.find(query)
+            .populate('usuario', 'nombre')
+            .skip( Number( desde ) )
+            .limit(Number( limite ))
+    ]);
+
+    res.json({
+        total,
+        categorias
+    });
+}
+
+const obtenerCategoria = async(req, res = response ) => {
+
+    const { id } = req.params;
+    const categoria = await Categoria.findById( id )
+                            .populate('usuario', 'nombre');
+
+    res.json( categoria );
+
+}
+
+
 const crearCategoria = (req, res = response) => {
 
     const nombre = req.body.nombre.toUpperCase();
