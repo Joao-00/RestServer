580b2ccd1c070876e39175e2961a76763881a721
Joao Ormeño <joao23.1996@gmail.com>
2022-05-30 18:36:49 -0400

    armando uploads

---
+++ routes/uploads.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { validarCampos } = require('../middlewares');
+
+
+
+const router = Router();
+
+
+
+
+
+
+module.exports = router;
