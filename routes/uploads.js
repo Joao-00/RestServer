f934cd7113405c10f86ee8bfeb1f31484bc60f2d
Joao Ormeño <joao23.1996@gmail.com>
2022-06-06 20:56:55 -0400

    agregando cloudinary

--- routes/uploads.js
+++ routes/uploads.js
@@ -18,7 +18,8 @@ router.put('/:coleccion/:id', [
     check('id','El id debe de ser de mongo').isMongoId(),
     check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios','productos'] ) ),
     validarCampos
- ], actualizarImagen )
+ ], actualizarImagenCloudinary )
+ //], actualizarImagen )
 
  router.get('/:coleccion/:id', [
     check('id','El id debe de ser de mongo').isMongoId(),
